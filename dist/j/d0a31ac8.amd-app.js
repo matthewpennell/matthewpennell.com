d6361f551e48b06ff7f258a4f88a55e5042f0b20
Matthew Pennell <github@matthewpennell.com>
2012-10-23 21:42:31 +0100

    Update colours, start using Bootstrap layout classes, add background tile

--- dist/j/d0a31ac8.amd-app.js
+++ dist/j/d0a31ac8.amd-app.js
@@ -2053,21 +2053,41 @@ var requirejs, require, define;
 }(this));
 
 (function () {
-define('app',[], function() {
-  return 'Hello from Yeoman!';
+/**
+ * This is the primary workhorse JavaScript code for matthewpennell.com.
+ * It is loaded by main.js via Require.js.
+ * 
+ * Within the define() call, the first parameter is an array of strings representing
+ * paths to dependencies, expressed without a suffix (e.g. ['./ajax']). These objects are
+ * then made available within the function by passing them as named parameters (e.g. function (ajax) {...}; )
+ * 
+ * The return value represents the object, value or function to be used witin main.js.
+ */
+
+define('app',[], function () {
+	// Basic click interception
+	$('nav a').click(function () {
+		var url = this.href;
+		$.ajax(url, {
+			error: function (obj, status, error) {
+				// TODO: Add error handling code here.
+			},
+			success: function (data, status, obj) {
+				$('.content').html($(data).filter('.content'));
+				history.pushState({}, '', url);
+			}
+		});
+		return false;
+	});
 });
 require.config({
-  shim: {
-  },
-
   paths: {
     jquery: 'vendor/jquery.min'
   }
 });
  
-require(['app'], function(app) {
-  // use app here
-  console.log(app);
+require(['app'], function (app) {
+  
 });
 define("main", function(){});
 }());
